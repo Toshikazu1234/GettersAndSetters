70b560eaf1f0452d6874889af12c20281759918c
RK <kanno.media808@gmail.com>
2024-02-05 14:30:49 -0800

    Setup project.

---
+++ GettersAndSetters/TestRow.swift
@@ -0,0 +1,26 @@
+//
+//  TestRow.swift
+//  GettersAndSetters
+//
+//  Created by R K on 2/5/24.
+//
+
+import UIKit
+
+final class TestRow: UITableViewCell {
+    static let id = "TestRow"
+
+    @IBOutlet private weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func configure(_ data: String) {
+        nameLabel.text = data
+    }
+}
--- GettersAndSetters/ViewController.swift
+++ GettersAndSetters/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+final class ViewController: UIViewController {
+    
+    private var names: [String] = [] {
+        didSet {
+            tableView.reloadData()
+        }
     }
 
-
+    @IBOutlet private weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        fetchNames()
+    }
+    
+    private func fetchNames() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+            guard let self else { return }
+            names = ["Bob", "John", "Sally", "Sarah", "Debby", "Carl", "Alex", "Natalie", "Sam", "Chuck", "Andy", "Lisa"]
+        }
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: TestRow.id,
+            for: indexPath) as! TestRow
+        cell.configure(names[indexPath.row])
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return view.frame.height * 0.22
+    }
+}
